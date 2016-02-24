f85354a44df2a109608021ec43cd383db40ce40f
John Senft <jsenft2001@yahoo.com>
2016-02-24 12:20:19 -0700

    Inital commit

---
+++ src/js/app.js
@@ -0,0 +1,20 @@
+// hard coded places data
+var model = {
+    places: [
+    {
+        // place data goes here
+    },
+    {
+        // place data goes here
+    }
+]};
+
+// Place constructor
+var Place = function(data) {
+    // properties, functionality etc.
+};
+
+// ViewModel
+var ViewModel = function() {
+    // call the Place constructor here
+};
