90c526b9e7ed9fc35c26b7c784b3893585bbbca7
John Senft <jsenft2001@yahoo.com>
2016-03-17 15:00:45 -0600

    Moved initMap function into viewModel; list, map, markers all working

--- src/js/app.js
+++ src/js/app.js
@@ -1,40 +1,42 @@
-var places = [
-    {
-        name      : "Adelitas Cocina Y Cantina",
-        lat       : "39.693269",
-        lng       : "-104.987079"
-    },
-    {
-        name      : "The Hornet",
-        lat       : "39.718130",
-        lng       : "-104.987261"
-    },
-    {
-        name      : "Sushi Den",
-        lat       : "39.689548",
-        lng       : "-104.980744"
-    },
-    {
-        name      : "Kaos Pizzaria",
-        lat       : "39.69048",
-        lng       : "-104.980636"
-    },
-    {
-        name      : "Park Burger Pearl",
-        lat       : "39.682267",
-        lng       : "-104.980374"
-    },
-    {
-        name      : "Gaia Bistro",
-        lat       : "39.688423",
-        lng       : "-104.980606"
-    },
-    {
-        name      : "Devils Food",
-        lat       : "39.697947",
-        lng       : "-104.961488"
-    }
-];
+var model = {
+    places : [
+        {
+            name      : 'Adelitas Cocina Y Cantina',
+            lat       : 39.693269,
+            lng       : -104.987079
+        },
+        {
+            name      : 'The Hornet',
+            lat       : 39.718130,
+            lng       : -104.987261
+        },
+        {
+            name      : 'Sushi Den',
+            lat       : 39.689548,
+            lng       : -104.980744
+        },
+        {
+            name      : 'Kaos Pizzaria',
+            lat       : 39.69048,
+            lng       : -104.980636
+        },
+        {
+            name      : 'Park Burger Pearl',
+            lat       : 39.682267,
+            lng       : -104.980374
+        },
+        {
+            name      : 'Gaia Bistro',
+            lat       : 39.688423,
+            lng       : -104.980606
+        },
+        {
+            name      : 'Devils Food',
+            lat       : 39.697947,
+            lng       : -104.961488
+        }
+    ]
+};
 
 
 var Place = function(data) {
@@ -47,52 +49,42 @@ var myViewModel = function() {
 
     var self = this;
 
-    this.placeList = ko.observableArray([]);
-    this.places.forEach(function(item) {
+    this.placeList = ko.observableArray();
+    model.places.forEach(function(item) {
         self.placeList.push(new Place(item));
     });
 
-    this.currentPlace = ko.observable(this.placeList()[0]);
-    this.setCurrentPlace = function(e) {
-        self.currentPlace(e);
-    }
+    initMap = function() {
 
-}
-
-function initMap() {
-
-    var denCenter = {lat: 39.695, lng: -104.991};
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: denCenter,
-        zoom: 14,
-    });
-
-    var infowindow = new google.maps.InfoWindow();
-    var marker, i, currentMarker;
-
-        for ( i = 0; i < self.places.length; i++) {
-            var data = self.places[i];
-            var location = new google.maps.LatLng(data.lat, data.lng);
-            marker = new google.maps.Marker( {
-                map: map,
-                position: location,
-                title: data.name
-            });
-        }
-        marker.addListener('click', function() {
-            currentMarker = this.marker;
-            infowindow.open(map, this.marker);
+        var denCenter = {lat: 39.695, lng: -104.991};
+        var map = new google.maps.Map(document.getElementById('map'), {
+            center: denCenter,
+            zoom: 14,
         });
-        function toggleBounce() {
-            if (marker.getAnimation() !== null) {
-                marker.setAnimation(null);
-            } else {
-                marker.setAnimation(google.maps.Animation.BOUNCE);
+
+        var infowindow = new google.maps.InfoWindow();
+        var marker, i, currentMarker;
+
+            for ( i = 0; i < this.model.places.length; i++) {
+                var data = this.model.places[i];
+                var location = new google.maps.LatLng(data.lat, data.lng);
+                marker = new google.maps.Marker( {
+                    map: map,
+                    position: location,
+                    title: data.name
+                });
             }
-        }
+            marker.addListener('click', function() {
+                currentMarker = this.marker;
+                infowindow.open(map, this.marker);
+            });
+            function toggleBounce() {
+                if (marker.getAnimation() !== null) {
+                    marker.setAnimation(null);
+                } else {
+                    marker.setAnimation(google.maps.Animation.BOUNCE);
+                }
+            }
+    }
 }
-
-
-var myVM = new myViewModel();
-initMap();
-ko.applyBindings(myVM);
+ko.applyBindings(new myViewModel());
