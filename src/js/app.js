9266b1d3399c1e9e41186b3af6ecd2bf47eef62d
John Senft <jsenft2001@yahoo.com>
2016-05-08 13:24:24 -0600

    Implemented Bootstrap

--- src/js/app.js
+++ src/js/app.js
@@ -1,106 +1,177 @@
 var places = [
     {
-        name      : 'Adelitas Cocina Y Cantina',
+        name      : 'Adelitas Cocina y Cantina',
+        id        : 'B5GgKhPREBgipv8kfSOluw',
+        web       : 'http://www.adelitasdenver.com/',
         latlng    : {lat: 39.693269, lng: -104.987079}
     },
     {
         name      : 'The Hornet',
+        id        : 'QmgLDVzZl_91e3-GceV-5w',
+        web       : 'http://hornetrestaurant.com/',
         latlng    : {lat: 39.718130, lng: -104.987261}
     },
     {
         name      : 'Sushi Den',
+        id        : '8lKL5Bxt620aqh0ulDByIg',
+        web       : 'http://www.sushiden.net/',
         latlng    : {lat: 39.689548, lng: -104.980744}
     },
     {
         name      : 'Kaos Pizzaria',
+        id        : 't2xnzRv93FTEvDnWQxYBkg',
+        web       : 'https://kaospizzeria.com/',
         latlng    : {lat: 39.69048, lng: -104.980636}
     },
     {
         name      : 'Park Burger Pearl',
+        id        : 'JSNizJSuj0gZ594nBXhVWQ',
+        web       : 'http://www.parkburger.com/',
         latlng    : {lat: 39.682267, lng: -104.980374}
     },
     {
         name      : 'Gaia Bistro',
+        id        : 'NztM8WhkcZiHyn_NGqWN5Q',
+        web       : 'http://www.gaiabistro.com/',
         latlng    : {lat: 39.688423, lng: -104.980606}
     },
     {
-        name      : 'Devils Food',
+        name      : 'Devils Food Bakery & Cookery',
+        id        : '4DE8RY7jkako7ADqpnAd3w',
+        web       : 'http://www.devilsfooddenver.com/',
         latlng    : {lat: 39.697947, lng: -104.961488}
     }
 ];
 
-var map, infWin, marker;
-
-function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 39.695, lng: -104.991},
-        zoom: 14,
-    });
-}
-
-// function Place(item) {
-//     this.name = ko.observable(item.name);
-//     this.latlng = ko.observable(item.latlng);
-// }
-
-function myViewModel() {
+var myViewModel = function() {
     var self = this;
+    var map, infWin, marker, currentMarker;
     self.markers = [];
-
-    // self.placeList = ko.observableArray();
-    // places.forEach(function(item) {
-    //     self.placeList.push(new Place(item));
-    // });
-
-    // self.currentPlace = ko.observable(self.placeList()[0]);
-    // self.setCurrentPlace = function(item) {
-    //     self.currentPlace(item);
-    // }
-
+    self.query = ko.observable('');
     self.allPlaces = ko.observableArray(places);
 
-    self.allPlaces().forEach(function(item) {
-        marker = new google.maps.Marker( {
-            map: map,
-            position: item.latlng,
-            title: item.name
-        });
-        item.marker = marker;
-        this.markers.push(marker);
-    });
+    var mapCenter = {lat: 39.695291, lng: -104.979265};
+    var initMap = function() {
+      map = new google.maps.Map(document.getElementById('map_canvas'), {
+          center: mapCenter,
+          zoom: 13
+      });
 
-    self.markers.map(function(item) {
-        infWin = new google.maps.InfoWindow({
-            content: "Test",
-            title: item.name
-        });
-        item.addListener('click', function() {
-            infWin.open(map, this),
-            item.setAnimation(google.maps.Animation.BOUNCE);
+      var bounds = new google.maps.LatLngBounds({ lat: 39.678447, lng: -104.993143 }, { lat: 39.725502, lng: -104.959331 });
+
+      google.maps.event.addDomListener(window, 'resize', function(){
+          map.fitBounds(bounds);
+          map.setZoom(13);
+      });
+
+      google.maps.event.addListener(map,'click', function() {
+          infWin.close();
+          map.setCenter(mapCenter);
+      });
+
+      self.allPlaces().forEach(function(item) {
+          marker = new google.maps.Marker( {
+              map: map,
+              position: item.latlng,
+              title: item.name
+          });
+          marker.id = item.id;
+          marker.web = item.web;
+          item.marker = marker;
+          self.markers.push(marker);
+      });
+
+      self.markers.map(function(item) {
+          infWin = new google.maps.InfoWindow({
+              maxWidth: 260
+          });
+          function yelpMe(info) {
+              return function(info) {
+                  item.setAnimation(google.maps.Animation.BOUNCE);
+                  setTimeout(function() {
+                      item.setAnimation(null)
+                  }, 1500);
+                  currentMarker = item;
+                  getYelp(this.id);
+              }
+          }
+          item.addListener('click', yelpMe(infWin));
+      });
+
+      self.listClick = function(item) {
+          if (this.name) {
+            item.marker.setAnimation(google.maps.Animation.BOUNCE);
             setTimeout(function() {
-                item.setAnimation(null)
+                item.marker.setAnimation(null)
             }, 1500);
-        });
-    });
+            currentMarker = item.marker;
+            getYelp(item.id);
+            }
+      }
 
-    self.listClick = function(item) {
-        if (this.name) {
-          item.marker.setAnimation(google.maps.Animation.DROP);
-          infWin.open(map, item.marker);
-        }
-    }
+      self.search = ko.computed(function() {
+          infWin.close();
+          return ko.utils.arrayFilter(self.allPlaces(), function(location) {
+              if (location.name.toLowerCase().indexOf(self.query().toLowerCase()) >= 0) {
+                  location.marker.setVisible(true);
+                  return true;
+              } else {
+                  location.marker.setVisible(false);
+                  return false;
+              }
+          });
+      }, self);
 
-    self.query = ko.observable('');
+      function getYelp(placeId) {
+          function nonce_generate() {
+              return (Math.floor(Math.random() * 1e12).toString());
+          };
 
-    self.search = ko.computed(function() {
-        return ko.utils.arrayFilter(self.allPlaces(), function(listResult) {
-            return listResult.name.toLowerCase().indexOf(self.query().toLowerCase()) >= 0;
-        });
-    });
+          var parameters = {
+              oauth_consumer_key: 'PHjRa8XxHyGkomr4Pk3CVQ',
+              oauth_token: 'vcZOZB6BjS3ENdun266bc05nAxlBOKWz',
+              oauth_nonce: nonce_generate(),
+              oauth_timestamp: Math.floor(Date.now() / 1000),
+              oauth_signature_method: 'HMAC-SHA1',
+              callback: 'cb',
+          }
+          var url = 'https://api.yelp.com/v2/business/' + placeId;
+          var encodedSignature = oauthSignature.generate('GET', url, parameters, 'GfVqiQ93A_VPJ6Ir5H93FYBJtkE', 'vk1WWobdGt7kfeXPiS9YWt0NGZo');
+          parameters.oauth_signature = encodedSignature;
+
+          var settings = {
+              url: url,
+              data: parameters,
+              cache: true,
+              dataType: 'jsonp',
+              timeout: 1000,
+              success: function(results) {
+                  var infoContent = '<div id="infwin" class="col-md-9"><h3>' + results.name + '</h3>' + '<div><img src="' + results.rating_img_url + '"></div>' + '<h4>' + results.location.address + '</h4><h4>' + results.display_phone + '</h4><a href="' + currentMarker.web + '" target="_blank">' + currentMarker.web + '</a><hr><div id="placeImg"><img src="' + results.image_url + '" class="img-responsive"></div>' + '<div id="reviewText">' + results.snippet_text + '</div><hr><div><a href="' + results.url + '" target="_blank">Go to Yelp Page</a></div><div><img src="img/yelp_powered_btn_red.png"></div></div>';
+                  infWin.setContent(infoContent);
+                  infWin.open(map, currentMarker);
+              },
+              error: function() {
+                  infWin.setContent("Sorry, No Yelp Information Available");
+                  infWin.open(map, currentMarker);
+              }
+          }
+          $.ajax(settings);
+      }
+  }();
 }
 
-// ko.applyBindings(new myViewModel());
-
-$(document).ready(function() {
-    ko.applyBindings(myViewModel);
-});
+function googleError() {
+    $("h2").hide();
+    $("p").hide();
+    $("input").hide();
+    $("li").hide();
+    $("header").append('<h1>Sorry, Google Maps has failed to load!</h1>');
+}
+function googleSuccess() {
+    if (typeof google !== 'undefined' || google === null) {
+        ko.applyBindings(new myViewModel());
+    }
+    else {
+        googleError();
+    }
+}
